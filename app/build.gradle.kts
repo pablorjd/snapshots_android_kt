931e9ae35ba8becde5b214e01ca9b84aab956a95
pablorjd1 <pablo.jimenez2221@gmail.com>
2023-11-05 10:53:47 -0300

    add firebase config, create dataclass

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
 
     id("kotlin-kapt")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -54,4 +55,22 @@ dependencies {
     // Glide
     implementation ("com.github.bumptech.glide:glide:4.14.2")
     kapt ("com.github.bumptech.glide:compiler:4.14.2")
+
+
+    //implementation("com.google.firebase:firebase-storage:20.3.0")
+
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    // Add the dependency for the Firebase SDK for Google Analytics
+    implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation("com.google.firebase:firebase-storage")
+    // FirebaseUI for Firebase Realtime Database
+    implementation ("com.firebaseui:firebase-ui-database:8.0.2")
+    // FirebaseUI for Cloud Firestore
+    implementation ("com.firebaseui:firebase-ui-firestore:8.0.2")
+    // FirebaseUI for Firebase Auth
+    implementation ("com.firebaseui:firebase-ui-auth:8.0.2")
+    // FirebaseUI for Cloud Storage
+    implementation ("com.firebaseui:firebase-ui-storage:8.0.2")
 }
---
+++ app/src/main/java/space/pablorjd/snapshots/Snapshot.kt
@@ -0,0 +1,12 @@
+package space.pablorjd.snapshots
+
+import com.google.firebase.database.IgnoreExtraProperties
+
+
+@IgnoreExtraProperties
+data class Snapshot(
+    var id: String = "",
+    var title: String = "",
+    var photoUrl: String = "",
+    var likeList: Map<String, Boolean> = mutableMapOf()
+)
