61ab7566ee1cda5c05ab1536729ea1211298879b
pablorjd1 <pablo.jimenez2221@gmail.com>
2023-11-17 22:14:32 -0300

    Se genera viewholder con la ayuda de firebaseui

--- app/src/main/java/space/pablorjd/snapshots/HomeFragment.kt
+++ app/src/main/java/space/pablorjd/snapshots/HomeFragment.kt
@@ -1,20 +1,86 @@
 package space.pablorjd.snapshots
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import space.pablorjd.snapshots.databinding.FragmentHomeBinding
+import space.pablorjd.snapshots.databinding.ItemSnapshotBinding
 
 
 class HomeFragment : Fragment() {
-
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var mFirebaseAdapter: FirebaseRecyclerAdapter<Snapshot,SnapshotHolder>
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val query = FirebaseDatabase.getInstance().reference.child("snapshots")
+        val options = FirebaseRecyclerOptions.Builder<Snapshot>().setQuery(query,Snapshot::class.java).build()
+
+        mFirebaseAdapter = object : FirebaseRecyclerAdapter<Snapshot,SnapshotHolder>(options) {
+            private lateinit var mContext: Context
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SnapshotHolder {
+                mContext = parent.context
+                val view = LayoutInflater.from(mContext).inflate(R.layout.item_snapshot,parent,false)
+
+                return SnapshotHolder(view)
+            }
+
+            override fun onBindViewHolder(holder: SnapshotHolder, position: Int, model: Snapshot) {
+                val snapshot = getItem(position)
+
+                with(holder) {
+                    setListener(snapshot)
+
+                    binding.tvTitle.text = snapshot.title
+                    Glide.with(mContext)
+                        .load(snapshot.photoUrl)
+                        .centerCrop()
+                        .diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .into(binding.imgPhoto)
+                }
+            }
+
+            override fun onDataChanged() {
+                super.onDataChanged()
+                binding.progresBar.visibility = View.GONE
+            }
+
+            override fun onError(error: DatabaseError) {
+                super.onError(error)
+                Toast.makeText(mContext, error.message, Toast.LENGTH_LONG).show()
+            }
+
+        }
+    }
+
+
+
+    inner class SnapshotHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val binding = ItemSnapshotBinding.bind(view)
+
+        fun setListener(snapshot: Snapshot) {
+
+        }
     }
 
 
